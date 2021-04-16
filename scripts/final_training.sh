2e89c9a05aae2258672ddda0611c42eaefd42efa
Julien Fischer <jufi2112@users.noreply.github.com>
2021-04-17 00:19:58 +0200

    added shell script to start final training

---
+++ scripts/final_training.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $CONDA_DEFAULT_ENV != "gaea" ]; then
+	echo "Activating conda environment 'gaea'"
+	source ~/anaconda3/etc/profile.d/conda.sh
+	conda activate gaea
+fi
+python /home/julienf/git/gaea_release/cnn/train_final.py \
+    run.seed=95 \
+    train.drop_path_prob=0.3 \
+    run.genotype_path=/home/julienf/data/genotype.json \
+    run.genotype_id=genotype_log &> /home/julienf/log_training.txt
