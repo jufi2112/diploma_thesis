09aae56c5510157d42017d64e8d4299de73a5619
Julien Fischer <jufi2112@users.noreply.github.com>
2021-05-10 13:10:36 +0200

    modified taurus job to also specify output file names

--- scripts/taurus_job.sh
+++ scripts/taurus_job.sh
@@ -6,10 +6,12 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=5772M
-#SBATCH -J "grid_search_sequential_seed_2554"
+#SBATCH -J "grid_search_sequential_seed_1431"
 #SBATCH --mail-user=julien.fischer@mailbox.tu-dresden.de
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -A p_da_studenten
+#SBATCH -o /scratch/ws/1/s8732099-da/slurm_output/grid_search_sequential_seed_1431_normal.out
+#SBATCH -e /scratch/ws/1/s8732099-da/slurm_output/grid_search_sequential_seed_1431_error.out
 
 module load modenv/ml
 module load torchvision/0.7.0-fosscuda-2019b-Python-3.7.4-PyTorch-1.6.0
@@ -19,8 +21,8 @@ export PYTHONPATH=$PYTHONPATH:/scratch/ws/1/s8732099-da/git/gaea_release/AutoDL-
 python /scratch/ws/1/s8732099-da/git/gaea_release/cnn/experiments_da.py \
     mode=grid_search \
     method.mode=sequential \
-    run_search_phase.seed=2554 \
-    run_eval_phase.seed=2554 \
+    run_search_phase.seed=1431 \
+    run_eval_phase.seed=1431 \
     run_search_phase.data=/scratch/ws/1/s8732099-da/data \
     run_eval_phase.data=/scratch/ws/1/s8732099-da/data \
     run_search_phase.autodl=/scratch/ws/1/s8732099-da/git/gaea_release/AutoDL-Projects \
