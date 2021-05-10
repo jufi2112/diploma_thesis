f092d090115aec5b515d6f93b0bc5506fe6b1561
Julien Fischer <jufi2112@users.noreply.github.com>
2021-05-10 11:08:16 +0200

    modified yaml config for single pc, added bash script for single pc

---
+++ scripts/experiments_da.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+# bash script for local pc
+if [ $CONDA_DEFAULT_ENV != "gaea" ]; then
+        echo "Activating conda environment 'gaea'"
+        source ~/anaconda3/etc/profile.d/conda.sh
+        conda activate gaea
+fi
+
+python /home/julienf/git/gaea_release/cnn/experiments_da.py  \
+        mode=grid_search \
+        run_search_phase.seed=2554 \
+        run_search_phase.data=/home/julienf/data \
+        run_search_phase.autodl=/home/julienf/git/gaea_release/AutoDL-Projects \
+        run_eval_phase.seed=2554 \
+        run_eval_phase.data=/home/julienf/data \
+        run_eval_phase.autodl=/home/julienf/git/gaea_release/AutoDL-Projects \
+        hydra.run.dir=/home/julienf/data/experiments_da/${method.name} &>> /home/julienf/log_experiments_da.txt
